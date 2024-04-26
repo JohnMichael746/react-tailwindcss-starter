88f4715ccb83dcdbd5b99fd15975e00320120709
johnmichael746 <withyou746@gmail.com>
2024-04-26 14:48:34 +0200

    init

---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { BeakerIcon } from '@heroicons/react/24/solid'
+
+export default function Home() {
+  return (
+    <div>
+      Hello
+      <BeakerIcon className="w-6 h-6 text-blue-500" />
+    </div>
+  );
+}
